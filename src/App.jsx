e227a496cab70bfd82912399de0a8f4b76854441
CodeWithInferno <infernotech2618@gmail.com>
2025-10-23 23:45:42 -0400

    Revert "fix: Remove redundant titlebar causing layout issues"
    This reverts commit 6586ac195b6a146db7c1b88b993b78a9d1c6d32f.

--- src/App.jsx
+++ src/App.jsx
@@ -97,6 +97,9 @@ function App() {
 
   return (
     <div className="app-root">
+      {/* Universal titlebar with drag region for all views */}
+      <div className="app-titlebar" data-tauri-drag-region></div>
+
       <div className="app-content">
         <AuthProvider>
           <GmailProvider>
